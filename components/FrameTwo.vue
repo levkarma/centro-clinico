7c86c6c1f6368b227113da7c08be6dd082dc707b
Levko Karmazyn <levkarma@vinmediagroup.com>
2020-02-28 19:20:21 -0500

    nuxt link frame two

--- components/FrameTwo.vue
+++ components/FrameTwo.vue
@@ -4,7 +4,10 @@
 			<figure class="image is-1by1" data-aos="fade-down">
 				<picture>
 					<source srcset="~/assets/img/logo.png" type="image/png" />
-					<source srcset="~/assets/img/logo.png.webp" type="image/webp" />
+					<source
+						srcset="~/assets/img/logo.png.webp"
+						type="image/webp"
+					/>
 					<img src="~/assets/img/logo.png" alt="" />
 				</picture>
 			</figure>
@@ -12,23 +15,24 @@
 		<h1 data-aos="fade-down" class="is-size-2">
 			Book an appointment or walk in today!
 		</h1>
-		<button
+		<nuxt-link
+			to="/locations"
 			data-aos="fade-down"
 			class="button is-large is-light has-top-margin"
 		>
-			{{ $t('header.locations') }}
-		</button>
+			{{ $t("header.locations") }}
+		</nuxt-link>
 	</section>
 </template>
 
 <script>
 export default {
-	name: 'FrameTwo',
+	name: "FrameTwo",
 
 	data() {
-		return {}
+		return {};
 	}
-}
+};
 </script>
 
 <style lang="css" scoped></style>
